7516a4d0cec26a0d233fac63bdc4520c9f6ce256
Daniel Chalef <daniel.chalef@gmail.com>
2015-04-26 17:57:14 -0400

    Added video

---
+++ raspi_video/raspicam_gs_video_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+# Thanks to Paul Ruvolo
+# Original: https://github.com/paulruvolo/CompRoboPrep.git
+
+VIDEOMODE="-ex sports -h 480 -w 640 -fps 15 -mm matrix"
+echo $#
+if [ $# = 1 ]; then
+	VIDEOMODE=$1
+	echo $VIDEOMODE
+fi
+
+while true; do
+	_IP=$(hostname -I) || true
+	echo $_IP
+	(raspivid -n --vflip -t 99999999 $VIDEOMODE -hf -b 2000000 -o - |  gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=$_IP port=5001) || true
+	sleep 2
+done
